50b18b8821ccbe171e50351d937b49758854dccf
PhoenixKing2501 <utsavbasu@outlook.com>
2022-09-04 20:50:12 +0530

    spandan wrote main

--- Week 3/main.c
+++ Week 3/main.c
@@ -1,6 +1,56 @@
-#include <stdio.h>
 #include "def.h"
+#include <stdio.h>
 
 int main()
 {
+	int token;
+
+	while (token = yylex())
+	{
+		switch (token)
+		{
+		case IDENTIFIER:
+			printf("< IDENTIFIER , %s >", yytext);
+			break;
+
+		case INT_CONST:
+			printf("< INTEGER CONSTANT , %s >", yytext);
+			break;
+
+		case FLOAT_CONST:
+			printf("< FLOAT CONSTANT , %s >", yytext);
+			break;
+
+		case ENUM_CONST:
+			printf("< ENUMERATION CONSTANT , %s >", yytext);
+			break;
+
+		case CHAR_CONST:
+			printf("< CHARACTER CONSTANT , %s >", yytext);
+			break;
+
+		case STRING_LITERAL:
+			printf("< STRING LITERAL , %s >", yytext);
+			break;
+
+		case PUNCTUATOR:
+			printf("< PUNCTUATOR , %s >", yytext);
+			break;
+
+		case LINE_COMMENT:
+			printf("< LINE COMMENT , %s >", yytext);
+			break;
+
+		case BLOCK_COMMENT:
+			printf("< BLOCK COMMENT , %s >", yytext);
+			break;
+
+		case KEYWORD:
+			printf("< KEYWORD , %s >", yytext);
+			break;
+
+		default:
+			break;
+		}
+	}
 }
